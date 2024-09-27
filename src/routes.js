e06943d64929281880e28bdd4291e2509ac95d1d
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2024-09-27 03:51:36 -0300

    :sparkles:	feat: Error page

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Movie from './pages/Movie';
 import Header from "./components/Header";
+import Error from "./pages/Error";
 
 function RoutesApp() {
     return(
@@ -11,6 +12,7 @@ function RoutesApp() {
             <Routes>
                 <Route path={'/'} element={ <Home/> } />
                 <Route path={'/movie/:id'} element={ <Movie/> } />
+                <Route path={'*'} element={ <Error/> } />
             </Routes>
         </BrowserRouter>
     )
